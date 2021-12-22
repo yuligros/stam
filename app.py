054a6807ce28c8550728b77d5b1a2fe4087f43a0
yuligros <yuligros11@gmail.com>
2021-12-22 22:18:06 +0200

    Task 9

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.secret_key = '123'
 app.permanent_session_lifetime = 30
 
+
 # ------------- DATABASE CONNECTION --------------- #
-# ------------------------------------------------- #
 def interact_db(query, query_type: str):
     return_value = False
     connection = mysql.connector.connect(
@@ -35,7 +35,7 @@ def interact_db(query, query_type: str):
     cursor.close()
     return return_value
 # ------------------------------------------------- #
-# ------------------------------------------------- #
+
 
 @app.route('/users')
 def func_users():
@@ -71,22 +71,10 @@ def hobbies_func():
     return render_template('assignment8.html',
                            hobbies = ['Basketball','Snowboard','Running','Reformer pilates','hiking','cooking','Free - diving'])
 
-# Contact page
-@app.route('/contact')
-def contact_func():
-    if 'y_name' in request.args:
-          name = request.args['y_name']
-          email = request.args['y_email']
-          password = request.args['y_password']
-          return render_template('contact.html', user_name = name,user_email = email,user_password = password)
-    return render_template('contact.html')
 
 # Contact page
 @app.route('/Search',methods = ['GET','POST'])
 def search_func():
-    user_name,second_name = '',''
-    users = get_users()
-    if_exist = False
 
     if request.method == 'POST':
           user_name = request.form['y_name']
@@ -112,7 +100,7 @@ def search_func():
               if q.exist == 1:
                 session['user_name'] = user_name
                 session['logged_in'] = True
-                return render_template('welcome.html')
+                return render_template('assignment9.html', id = id, user_name = user_name)
            return render_template('users.html', users=query_reasult)
 
     return render_template('assignment9.html')
@@ -124,24 +112,6 @@ def logout():
     session['logged_in'] = False
     return redirect(url_for('home_func'))
 
-
-def get_users():
-    users = {'user1': {'user_name': 'yuli', 'email' : 'yuli11@gmail.com'},
-             'user2': {'user_name': 'yosik', 'email': 'yosik12@gmail.com'},
-             'user3': {'user_name': 'erez', 'email': 'erezMaster@gmail.com'},
-             'user4': {'user_name': 'amit', 'email': 'tuli@gmail.com'},
-             'user5': {'user_name': 'guy', 'email': 'guyTheKing@gmail.com'},
-             'user6': {'user_name': 'shahaf', 'email': 'shahafgros@gmail.com'},
-             'user7': {'user_name': 'dor', 'email': 'dori100@gmail.com'},
-             'user8': {'user_name': 'itamar', 'email': 'itamari@gmail.com'}}
-    return users
-
-
-# def check_if_user_exist(id,users):
-#     for for user in users:
-#             if user.id == id:
-#                 return True;
-#     return False
 
 
 if __name__ == '__main__':
